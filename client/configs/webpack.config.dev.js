7558495195e32c9c53f1d1d4599cd245f196608d
krazijames <krazijames@ridi.com>
2018-03-22 17:18:22 +0900

    Remove unnecessary object wrapping

--- client/configs/webpack.config.dev.js
+++ client/configs/webpack.config.dev.js
@@ -10,30 +10,28 @@ const defaultEntry = [
 
 module.exports = {
   ...config,
-  ...{
-    entry: _.mapValues(config.entry, value => _.flattenDeep([
-      defaultEntry,
-      value,
-    ])),
-    output: {
-      ...config.output,
-      filename: '[name].js',
+  entry: _.mapValues(config.entry, value => _.flattenDeep([
+    defaultEntry,
+    value,
+  ])),
+  output: {
+    ...config.output,
+    filename: '[name].js',
+  },
+  plugins: [
+    ...config.plugins,
+    new webpack.HotModuleReplacementPlugin(),
+    new WriteFilePlugin({
+      test: /manifest\.json$/,
+    }),
+  ],
+  devServer: {
+    publicPath: PUBLIC_PATH,
+    port: 3000,
+    proxy: {
+      '*': 'http://localhost:8012',
     },
-    plugins: [
-      ...config.plugins,
-      new webpack.HotModuleReplacementPlugin(),
-      new WriteFilePlugin({
-        test: /manifest\.json$/,
-      }),
-    ],
-    devServer: {
-      publicPath: PUBLIC_PATH,
-      port: 3000,
-      proxy: {
-        '*': 'http://localhost:8012',
-      },
-      inline: true,
-      hot: true,
-    },
+    inline: true,
+    hot: true,
   },
 };
