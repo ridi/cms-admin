e02c5bd56db7db094099e4b66850059642b9865e
krazijames <krazijames@ridi.com>
2018-04-10 17:48:34 +0900

    Move common components to components dir=

--- client/src/user_edit/UserPermissionForm.jsx
+++ client/src/user_edit/UserPermissionForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-import Select2Input from '../Select2Input';
+import Select2Input from '../components/Select2Input';
 
 class UserPermissionForm extends React.Component {
   constructor(props) {
