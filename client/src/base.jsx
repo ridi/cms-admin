3a5639679cc51fad255f299cf99ecb192dfc18d0
krazijames <krazijames@ridi.com>
2018-04-18 12:34:48 +0900

    Apply CMS UI (#39)
    * Use cms-ui's menu
    
    * Update cms-ui
    
    * Remove unused code

--- client/src/base.jsx
+++ client/src/base.jsx
@@ -1,19 +1,16 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'select2/dist/js/select2.full';
 import 'select2/dist/css/select2.min.css';
 
+import { Menu } from '@ridi/cms-ui';
 import './base.css';
 
-$(() => {
-  const $menuSelect = $('.menu_select');
-  $menuSelect.select2({
-    placeholder: '메뉴를 검색하세요',
-  });
-
-  $menuSelect.on('select2:select', (e) => {
-    window.location.href = e.params.data.id;
-  });
-});
+ReactDOM.render(
+  <Menu items={window.menuItems} />,
+  document.getElementById('menu_container'),
+);
