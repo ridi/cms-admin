60f5811f964476f13e79735f019572468f40f01a
krazijames <krazijames@ridi.com>
2018-04-18 12:11:09 +0900

    Remove unused code

--- client/src/base.jsx
+++ client/src/base.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,14 +14,3 @@ ReactDOM.render(
   <Menu items={window.menuItems} />,
   document.getElementById('menu_container'),
 );
-
-$(() => {
-  const $menuSelect = $('.menu_select');
-  $menuSelect.select2({
-    placeholder: '메뉴를 검색하세요',
-  });
-
-  $menuSelect.on('select2:select', (e) => {
-    window.location.href = e.params.data.id;
-  });
-});
