f23918916b9164f58792d86c41b8369d3c9a2b23
krazijames <krazijames@ridi.com>
2018-06-11 18:02:27 +0900

    Load cms-ui from CDN

--- client/src/base.jsx
+++ client/src/base.jsx
@@ -1,16 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'select2/dist/js/select2.full';
 import 'select2/dist/css/select2.min.css';
 
-import { Menu } from '@ridi/cms-ui';
 import './base.css';
-
-ReactDOM.render(
-  <Menu items={window.menuItems} />,
-  document.getElementById('menu_container'),
-);
