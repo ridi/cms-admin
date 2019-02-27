94dd1e433581cc2c9ea80dc3adc77c49140ee60a
Yeongjun Choi <yj.choi@ridi.com>
2019-02-27 21:42:45 +0900

    Added a create (post) methods function

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,5 +1,9 @@
-from rest_framework import generics
+import json
+
+from django.http import HttpResponse, JsonResponse
+from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.parsers import FormParser, JSONParser
 
 from .serializers import UserSerializer
 from .models import User
@@ -10,12 +14,24 @@ class UserListCreateAPIView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def list(self, request):
+    def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         serializer = UserSerializer(queryset, many=True)
         return Response(serializer.data)
 
-class UserDetailsAPIView(generics.RetrieveUpdateDestroyAPIView):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class UserDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    renderer_classes = (JSONRenderer, )
+
     pass
