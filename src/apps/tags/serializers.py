21fe93a1f7f7676de279343553a8d1271e26833a
Yeongjun Choi <yj.choi@ridi.com>
2019-03-04 19:18:04 +0900

    defined a creator field as foreign key with User model

--- src/apps/tags/serializers.py
+++ src/apps/tags/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Tag
+from apps.users.serializers import UserSerializer
 
 
-class TagSerializer(serializers.ModelSerializer):
+class TagSerializer(serializers.HyperlinkedModelSerializer):
+    creator =  UserSerializer(read_only=True, default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Tag
-        fields = ('id', 'display_name', 'is_use', 'creator', 'reg_date', 'created_at', 'updated_at')
+        fields = ('id', 'name', 'display_name', 'is_use', 'creator', 'created_at', 'updated_at')
+        # be added fields; inactive_tags, menus_count, active_tags
