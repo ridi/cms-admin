10402ac3a1f3bda3850888678c6cda5c35d2170f
Yeongjun Choi <yj.choi@ridi.com>
2019-03-04 18:43:12 +0900

    Added a TagListCreateAPIViewTestCase

--- src/apps/tags/tests.py
+++ src/apps/tags/tests.py
@@ -1,3 +1,73 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase, force_authenticate
+
+from apps.tags.models import Tag
+from apps.tags.serializers import TagSerializer
+
+
+class TagListCreateAPIViewTestCase(APITestCase):
+    def setUp(self):
+        self.url = reverse('tags:list')
+        self.tag_data = {
+            'name' : '태그이름',
+            'display_name' : '보이는 태그이름',
+            'is_use' : 1,
+        }
+        self.user_data = {
+            'id' : 'testuser',
+            'name' : '홍길동',
+            'email' : 'test@ridi.com',
+            'team' : '퍼포먼스팀',
+            'password' : 'this_is_secure_password!',
+            'is_use' : 1,
+        }
+        self.user, _ = get_user_model().objects.get_or_create(**self.user_data)
+        self.client.force_authenticate(user=self.user)
+
+    def test_can_create_tag(self):
+        response = self.client.post(self.url, self.tag_data, format='json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code, response.data)
+        except_data = TagSerializer(Tag.objects.get(name=self.tag_data['name'])).data
+        self.assertEqual(json.loads(response.content), except_data)
+
+    def test_get_list_tag(self):
+        self.tag = Tag.objects.create(**self.tag_data, creator=self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(json.loads(response.content), TagSerializer(Tag.objects.all(), many=True).data)
+
+class TagDetailAPIViewTestCase(APITestCase):
+    def setUp(self):
+        self.user_data = {
+            'id' : 'testuser',
+            'name' : '홍길동',
+            'email' : 'test@ridi.com',
+            'team' : '퍼포먼스팀',
+            'password' : 'this_is_secure_password!',
+            'is_use' : 1,
+        }
+        self.tag_data = {
+            'id' : 'testtag',
+            'name' : '홍길동',
+            'email' : 'test@ridi.com',
+            'team' : '퍼포먼스팀',
+            'password' : 'this_is_secure_password!',
+            'is_use' : 1,
+        }
+        self.user, _ = get_user_model().objects.get_or_create(**self.user_data)
+        self.client.force_authenticate(user=self.user)
+        self.tag = Tag.objects.create(**self.tag_data, creator=self.user)
+
+        self.url = reverse('tags:detail', kwargs={'pk': self.tag.id})
+
+    def test_can_show_specific_tag(self):
+        response = self.client.get(self.url, self.tag_data)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        except_data = TagSerializer(self.tag).data
+        self.assertEqual(json.loads(response.content), except_data)
