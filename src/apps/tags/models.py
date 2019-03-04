4c3620748a79fcadd14a5cfafb95bd5efb8c181c
Yeongjun Choi <yj.choi@ridi.com>
2019-03-04 18:42:39 +0900

    Changed creator field to foreignkey with User Model

--- src/apps/tags/models.py
+++ src/apps/tags/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+from django.contrib.auth.models import AbstractUser
 
 #class TbAdmin2Tag(models.Model):
 class Tag(models.Model):
     name = models.CharField(unique=True, max_length=32)
     display_name = models.CharField(max_length=32)
     is_use = models.IntegerField()
-    creator = models.CharField(max_length=32)
-    reg_date = models.DateTimeField()
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.PROTECT)
+    reg_date = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(blank=True, null=True)
 
