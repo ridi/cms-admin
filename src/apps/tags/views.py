28736de80f60fde0f2aeeae29da70196123f4e42
Yeongjun Choi <yj.choi@ridi.com>
2019-03-04 19:16:41 +0900

    Updated a TagDetailAPIView

--- src/apps/tags/views.py
+++ src/apps/tags/views.py
@@ -1,12 +1,13 @@
-from django.contrib.auth.models import User
 
-from rest_framework import generics
+from rest_framework import mixins, generics, status
 from rest_framework.response import Response
 
 from .serializers import TagSerializer
 from .models import Tag
 
 from rest_framework.renderers import JSONRenderer
+
+from django.contrib.auth import get_user_model
 
 class TagListCreateAPIView(generics.ListCreateAPIView):
     queryset = Tag.objects.all()
@@ -17,7 +18,14 @@ class TagListCreateAPIView(generics.ListCreateAPIView):
         serializer = TagSerializer(queryset, many=True)
         return Response(serializer.data)
 
-class TagDetailsAPIView(generics.RetrieveUpdateDestroyAPIView):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(creator=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+class TagDetailsAPIView(generics.DestroyAPIView, generics.UpdateAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     pass
