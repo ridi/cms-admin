ba8e6ed170a72a892cf46f280808c9ce5d5a413b
Yeongjun Choi <yj.choi@ridi.com>
2019-03-04 18:39:44 +0900

    Change parent class to abstract models

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-
-class User(models.Model):
+class User(AbstractUser):
     id = models.CharField(primary_key=True, max_length=32)
-    passwd = models.CharField(max_length=128)
+    #passwd = models.CharField(max_length=128)
+    password = models.CharField(max_length=128)
     email = models.CharField(max_length=128)
     name = models.CharField(max_length=32)
     team = models.CharField(max_length=32)
     is_use = models.IntegerField(default=1)
     reg_date = models.DateTimeField(auto_now_add=True)
 
+    REQUIRED_FIELDS = ['password']
+    USERNAME_FIELD = 'id'
     class Meta:
         db_table = 'tb_admin2_user'
