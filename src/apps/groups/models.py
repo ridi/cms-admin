6542aaf18fc87ad61c056ef8cad4835ae9ba176d
Yeongjun Choi <yj.choi@ridi.com>
2019-03-05 16:34:50 +0900

    Changed to creator field to foreign key

--- src/apps/groups/models.py
+++ src/apps/groups/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 
 class Group(models.Model):
     name = models.CharField(unique=True, max_length=32)
-    is_use = models.IntegerField()
-    creator = models.CharField(max_length=32)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField(blank=True, null=True)
+    is_use = models.IntegerField(default=1)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'tb_admin2_group'
