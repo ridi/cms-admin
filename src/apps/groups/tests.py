0dd2bad2182a334741165c48b56e76d3d7fd61d8
Yeongjun Choi <yj.choi@ridi.com>
2019-03-05 17:21:03 +0900

    Append a test case as GroupDetailsAPIViewTestCase

--- src/apps/groups/tests.py
+++ src/apps/groups/tests.py
@@ -46,3 +46,29 @@ class GroupListCreateAPIViewTestCase(APITestCase):
         response = self.client.post(self.url, self.group_data)
         self.assertEqual(status.HTTP_201_CREATED, response.status_code, response.data)
         self.assertTrue(Group.objects.filter(name=self.group_data['name']).exists())
+
+class GroupDetailsAPIViewTestCase(APITestCase):
+    def setUp(self):
+        self.fake = Faker('ko_KR')
+        self.group_data = {
+            'name' : self.fake.name() + '팀',
+            'is_use' : 1,
+        }
+        self.user_data = {
+            'id' : self.fake.user_name(),
+            'name' : self.fake.name(),
+            'email' : self.fake.email(),
+            'team' : self.fake.job() + '팀',
+            'password' : self.fake.password(),
+            'is_use' : 1,
+        }
+        self.user, _ = get_user_model().objects.get_or_create(**self.user_data)
+        self.client.force_authenticate(user=self.user)
+        self.group = Group.objects.create(**self.group_data, creator=self.user)
+        self.url = reverse('groups:detail', kwargs={'pk' : self.group.id})
+
+    def test_can_delete_specific_group(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertFalse(Group.objects.filter(id=self.group.id).exists())
+
