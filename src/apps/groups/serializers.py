492bea2eaa67a38091ce79ea5f78297c11d6f2b9
Yeongjun Choi <yj.choi@ridi.com>
2019-03-05 17:05:00 +0900

    Serialized a 'creator' field as UserSerializer

--- src/apps/groups/serializers.py
+++ src/apps/groups/serializers.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 from rest_framework import serializers
-from .models import Group
+
+from apps.groups.models import Group
+from apps.users.serializers import UserSerializer
 
 
 class GroupSerializer(serializers.ModelSerializer):
+    creator = UserSerializer(read_only=True, default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Group
-        fields = ('id', 'is_use', 'creator', 'created_at', 'updated_at')
+        fields = ('id', 'name', 'is_use', 'creator', 'created_at', 'updated_at')
