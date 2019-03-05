e360616ed0675d2acd7cbd44e41c2c5f65ad2f86
Yeongjun Choi <yj.choi@ridi.com>
2019-03-05 17:05:39 +0900

    Added a groups test cases

--- src/apps/groups/views.py
+++ src/apps/groups/views.py
@@ -1,21 +1,28 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
-
-from .serializers import GorupSerializer
-from .models import Group
-
 from rest_framework.renderers import JSONRenderer
+
+from apps.groups.serializers import GroupSerializer
+from apps.groups.models import Group
 
 class GroupListCreateAPIView(generics.ListCreateAPIView):
     queryset = Group.objects.all()
-    serializer_class = GorupSerializer
+    serializer_class = GroupSerializer
 
     def list(self, request):
         queryset = self.get_queryset()
-        serializer = GorupSerializer(queryset, many=True)
+        serializer = GroupSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(creator=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 class GroupDetailsAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Group.objects.all()
-    serializer_class = GorupSerializer
+    serializer_class = GroupSerializer
     pass
