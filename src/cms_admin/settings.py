d64048ffe086e251ba627195a3fbd67808d014f1
Yeongjun Choi <yj.choi@ridi.com>
2019-03-05 20:17:21 +0900

    Updated Timezone utc to kst(Asia/Seoul)

--- src/cms_admin/settings.py
+++ src/cms_admin/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'rest_framework',
     'apps.tags',
     'apps.groups',
@@ -133,7 +134,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
