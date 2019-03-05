e5f4279d2fbdc5ddfd306b2d623a17d8d9a4e377
Yeongjun Choi <yj.choi@ridi.com>
2019-03-05 17:07:18 +0900

    Added a test case with GroupListCreateAPIView

--- src/apps/groups/tests.py
+++ src/apps/groups/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+import json
+import logging
+from faker import Faker
 
-# Create your tests here.
+from django.urls import reverse
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from rest_framework.test import APITestCase, force_authenticate
+from rest_framework import status
+
+from apps.groups.models import Group
+from apps.groups.serializers import GroupSerializer
+
+# not printing "Faker/faker.factory" debug message as a stdout
+logging.getLogger('faker.factory').setLevel(logging.WARNING)
+
+
+class GroupListCreateAPIViewTestCase(APITestCase):
+    def setUp(self):
+        self.fake = Faker('ko_KR')
+        self.url = reverse('groups:list')
+        self.group_data = {
+            'name' : self.fake.name() + '팀',
+            'is_use' : 1,
+        }
+        self.user_data = {
+            'id' : self.fake.user_name(),
+            'name' : self.fake.name(),
+            'email' : self.fake.email(),
+            'team' : self.fake.job() + '팀',
+            'password' : self.fake.password(),
+            'is_use' : 1,
+        }
+        self.user, _ = get_user_model().objects.get_or_create(**self.user_data)
+        self.client.force_authenticate(user=self.user)
+
+    def test_can_list_groups(self):
+        self.group = Group.objects.create(**self.group_data, creator=self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code, response.data)
+        except_response = GroupSerializer(Group.objects.all(), many=True).data
+        self.assertEqual(json.loads(response.content), except_response)
+
+    def test_can_create_group(self):
+        response = self.client.post(self.url, self.group_data)
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code, response.data)
+        self.assertTrue(Group.objects.filter(name=self.group_data['name']).exists())
