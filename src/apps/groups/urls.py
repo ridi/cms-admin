342766ca388beb77cd632c9864f5dbfed57b3e04
Yeongjun Choi <yj.choi@ridi.com>
2019-03-05 17:06:28 +0900

    Removed a slash at end of path

--- src/apps/groups/urls.py
+++ src/apps/groups/urls.py
@@ -8,5 +8,5 @@ app_name = 'groups'
 
 urlpatterns = [
     path('', views.GroupListCreateAPIView.as_view(), name='list'),
-    path('<int:pk>/', views.GroupDetailAPIView.as_view(), name="detail"),
+    path('<int:pk>', views.GroupDetailsAPIView.as_view(), name="detail"),
 ]
--- src/apps/tags/urls.py
+++ src/apps/tags/urls.py
@@ -7,6 +7,6 @@ from apps.tags import views
 app_name = 'tags'
 
 urlpatterns = [
-    path('/', views.TagListCreateAPIView.as_view(), name='list'),
-    path('/<int:pk>/', views.TagDetailsAPIView.as_view(), name="detail"),
+    path('', views.TagListCreateAPIView.as_view(), name='list'),
+    path('<int:pk>', views.TagDetailsAPIView.as_view(), name="detail"),
 ]
