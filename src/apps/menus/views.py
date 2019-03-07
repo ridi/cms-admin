2a9cc3232dbbda12012711550470b394022e3551
Yeongjun Choi <yj.choi@ridi.com>
2019-03-07 16:59:19 +0900

    Implements a update method at MenuAPIView

--- src/apps/menus/views.py
+++ src/apps/menus/views.py
@@ -1,21 +1,35 @@
-from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
-
-from .serializers import MenuSerializer
-from .models import Menu
-
 from rest_framework.renderers import JSONRenderer
+from rest_framework.exceptions import ValidationError, ParseError
 
-class MenuListCreateUpdateAPIView(generics.GenericAPIView,
-                                  generics.ListModelMixin,
-                                  generics.CreateModelMixin,
-                                  generics.UpdateModelMixin):
+from apps.menus.serializers import MenuSerializer
+from apps.menus.models import Menu
+
+class MenuRetrieveUpdateAPIView(generics.ListCreateAPIView, generics.UpdateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
     def list(self, request):
         queryset = self.get_queryset()
-        serializer = MenuSerializer(queryset, many=True)
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        menus = list ()
+        request_data = request.data
+        if isinstance(request_data, dict):
+            request_data = [request_data]
+
+        for menu_data in request_data:
+            _id = menu_data.pop('id')
+            obj, _ = Menu.objects.update_or_create(id=_id, defaults=menu_data)
+            menus.append(obj)
+
+        serializer = self.get_serializer(menus, many=True)
+        return Response(serializer.data)
+
+    def get_object(self, id):
+        return Menu.objects.get_object_or_404(id=id)
