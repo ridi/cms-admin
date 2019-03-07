c37d3119e4ac45c8111ee156cba76cc52526d8d0
Yeongjun Choi <yj.choi@ridi.com>
2019-03-07 16:56:49 +0900

    Improved a Menu models as set a default value

--- src/apps/menus/models.py
+++ src/apps/menus/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
-
-class TbAdmin2Menu(models.Model):
-    menu_title = models.CharField(max_length=50)
+class Menu(models.Model):
+    menu_title = models.CharField(max_length=50, unique=True)
     menu_url = models.CharField(max_length=200)
     menu_deep = models.IntegerField()
     menu_order = models.IntegerField()
-    is_use = models.IntegerField()
-    is_show = models.IntegerField()
-    reg_date = models.DateTimeField()
+    is_use = models.IntegerField(default=1)
+    is_show = models.IntegerField(default=1)
     is_newtab = models.IntegerField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField(blank=True, null=True)
+    reg_date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'tb_admin2_menu'
